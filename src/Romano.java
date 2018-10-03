083e07e36992aed0bc5bffe34dc7029157caf29a
ricardocastilloisc <ricardocastilloisc@gmail.com>
2018-10-03 18:40:19 -0500

    banderas de repeticiones
    aqui se ponen la bandera donde podremos manipulas las condiciones

--- src/Romano.java
+++ src/Romano.java
@@ -1,6 +1,7 @@
 
 public class Romano {
 	
+	//la matriz donde estaran todos los numeros romanos
 	static String NumerosRomanos[][] = 
 		{
 				{ "I", "1" },
@@ -11,9 +12,31 @@ public class Romano {
 				{ "D", "500" },
 				{ "M", "1000" }
 		};
+	
+	//metodo donde se realizaran todas las operaciones
+	static public  String romano(String texto) 
+	{
+		//conteos donde estos valores
+		//hasta 3 veces se pueden repetir
+		int ConteoNumeroCompuestoI = 0;
+		int ConteoNumeroCompuestoX = 0;
+		int ConteoNumeroCompuestoC = 0;
+		int ConteoNumeroCompuestoM = 0;
+		//No se puden repetir nunca
+		int ConteoNumeroCompuestoV = 0;
+		int ConteoNumeroCompuestoL = 0;
+		int ConteoNumeroCompuestoD = 0;
+		
+		
+		return texto;
+	};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //empezando la aplicacion 
+		//cadena que se utilizara para todas la conversiones
+		String Cadena =  "xiv";
+		//convocando al metodo
+		System.out.println(romano(Cadena));
 	}
 
 }
